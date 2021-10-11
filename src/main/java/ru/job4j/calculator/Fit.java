f1b16203c6f8b53ed0d2a53d5b986fb56cf5a89e
Aalleexxaannddeer <Alakey1998ssss@mail.ru>
2021-10-11 12:43:01 +0300

    6. Идеальный вес [#41584]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,26 +1,22 @@
-# Job4j. Идеальный вес.
-Проект содержит решения блока "6. Идеальный вес [#41584]" курса Job4j."
-Задание:
-1. Доработал методы manWeight и womanWeight. Было:
 package ru.job4j.calculator;
 
 public class Fit {
 
     public static double manWeight(short height) {
-        double rsl = -1;
+        double rsl = (height - 100) * 1.15;
         return rsl;
     }
 
     public static double womanWeight(short height) {
-        double rsl = -1;
+        double rsl = (height - 110) * 1.15;
         return rsl;
     }
 
     public static void main(String[] args) {
         short height = 187;
         double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height);
         System.out.println("Man 187 is " + man);
+        System.out.println("Woman 187 is " + woman);
     }
-
 }
-2. Загрузил на github.com.
