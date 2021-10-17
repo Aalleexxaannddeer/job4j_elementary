5204902243d4e3b65cb2bf2b2f539880b391a2a1
Aalleexxaannddeer <Alakey1998ssss@mail.ru>
2021-10-17 13:55:43 +0300

    1.2. Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -5,10 +5,6 @@ public class X2 {
         return a * x * x + b * x + c;
     }
 
-    public static int calcOne(int a, int b, int c, int x) {
-        return a * x * x + b * x + c;
-    }
-
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
@@ -16,5 +12,29 @@ public class X2 {
         int x = 1;
         int rsl = X2.calc(a, b, c, x);
         System.out.println(rsl);
+        int aOne = 1;
+        int bOne = 1;
+        int cOne = 1;
+        int xOne = 1;
+        int rslOne = X2.calc(aOne, bOne, cOne, xOne);
+        System.out.println(rslOne);
+        int aTwo = 0;
+        int bTwo = 1;
+        int cTwo = 1;
+        int xTwo = 1;
+        int rslTwo = X2.calc(aTwo, bTwo, cTwo, xTwo);
+        System.out.println(rslTwo);
+        int aThree = 0;
+        int bThree = 1;
+        int cThree = 1;
+        int xThree = 1;
+        int rslThree = X2.calc(aThree, bThree, cThree, xThree);
+        System.out.println(rslThree);
+        int aFour = 0;
+        int bFour = 1;
+        int cFour = 1;
+        int xFour = 1;
+        int rslFour = X2.calc(aFour, bFour, cFour, xFour);
+        System.out.println(rslFour);
     }
 }
--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -21,6 +21,33 @@ public class X2Test {
         int rsl = X2.calc(a, b, c, x);
         /* срвнение полученного значения с ожидаемым */
         Assert.assertEquals(expected, rsl);
-
+        int aOne = 1;
+        int bOne = 1;
+        int cOne = 1;
+        int xOne = 1;
+        int expectedOne = 3;
+        int rslOne = X2.calc(aOne, bOne, cOne, xOne);
+        Assert.assertEquals(expectedOne, rslOne);
+        int aTwo = 0;
+        int bTwo = 1;
+        int cTwo = 1;
+        int xTwo = 1;
+        int expectedTwo = 2;
+        int rslTwo = X2.calc(aTwo, bTwo, cTwo, xTwo);
+        Assert.assertEquals(expectedTwo, rslTwo);
+        int aThree = 1;
+        int bThree = 1;
+        int cThree = 0;
+        int xThree = 1;
+        int expectedThree = 2;
+        int rslThree = X2.calc(aThree, bThree, cThree, xThree);
+        Assert.assertEquals(expectedThree, rslThree);
+        int aFour = 1;
+        int bFour = 1;
+        int cFour = 1;
+        int xFour = 0;
+        int expectedFour = 1;
+        int rslFour = X2.calc(aFour, bFour, cFour, xFour);
+        Assert.assertEquals(expectedFour, rslFour);
     }
 }
