603ad46cb69280798be4fbb900d7b928f367cb49
Aalleexxaannddeer <Alakey1998ssss@mail.ru>
2021-10-18 16:47:05 +0300

    3. Тесты для определения расстояния между точками. [#159459]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -6,8 +6,29 @@ public class Point {
         return rsl;
     }
 
+    public static double distanceOne(int x1One, int y1One, int x2One, int y2One) {
+        double rslOne = Math.sqrt(Math.pow(x2One - x1One, 2) + Math.pow(y2One - y1One, 2));
+        return rslOne;
+    }
+
+    public static double distanceTwo(int x1Two, int y1Two, int x2Two, int y2Two) {
+        double rslTwo = Math.sqrt(Math.pow(x2Two - x1Two, 2) + Math.pow(y2Two - y1Two, 2));
+        return rslTwo;
+    }
+
+    public static double distanceThree(int x1Three, int y1Three, int x2Three, int y2Three) {
+        double rslThree = Math.sqrt(Math.pow(x2Three - x1Three, 2) + Math.pow(y2Three - y1Three, 2));
+        return rslThree;
+    }
+
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
+        double resultOne = Point.distanceOne(0, 1, 1, 1);
+        System.out.println("resultOne (0, 1) to (1, 1) " + resultOne);
+        double resultTwo = Point.distanceTwo(0, 0, 3, 0);
+        System.out.println("resultTwo (0, 0) to (3, 0) " + resultTwo);
+        double resultThree = Point.distanceThree(2, 0, 3, 0);
+        System.out.println("resultThree (2, 0) to (3, 0) " + resultThree);
     }
 }
