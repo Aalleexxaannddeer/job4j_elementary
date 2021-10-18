f3574e2f4553d5725f3f0e63862c397d2a7434bc
Aalleexxaannddeer <Alakey1998ssss@mail.ru>
2021-10-18 13:04:05 +0300

    1.2. Входные данные в тесте [#297750 #217091]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -5,6 +5,22 @@ public class X2 {
         return a * x * x + b * x + c;
     }
 
+    public static int calcOne(int aOne, int bOne, int cOne, int xOne) {
+        return aOne * xOne * xOne + bOne * xOne + cOne;
+    }
+
+    public static int calcTwo(int aTwo, int bTwo, int cTwo, int xTwo) {
+        return aTwo * xTwo * xTwo + bTwo * xTwo + cTwo;
+    }
+
+    public static int calcThree(int aThree, int bThree, int cThree, int xThree) {
+        return aThree * xThree * xThree + bThree * xThree + cThree;
+    }
+
+    public static int calcFour(int aFour, int bFour, int cFour, int xFour) {
+        return aFour * xFour * xFour + bFour * xFour + cFour;
+    }
+
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
@@ -16,25 +32,25 @@ public class X2 {
         int bOne = 1;
         int cOne = 1;
         int xOne = 1;
-        int rslOne = X2.calc(aOne, bOne, cOne, xOne);
+        int rslOne = X2.calcOne(aOne, bOne, cOne, xOne);
         System.out.println(rslOne);
         int aTwo = 0;
         int bTwo = 1;
         int cTwo = 1;
         int xTwo = 1;
-        int rslTwo = X2.calc(aTwo, bTwo, cTwo, xTwo);
+        int rslTwo = X2.calcTwo(aTwo, bTwo, cTwo, xTwo);
         System.out.println(rslTwo);
-        int aThree = 0;
+        int aThree = 1;
         int bThree = 1;
-        int cThree = 1;
+        int cThree = 0;
         int xThree = 1;
-        int rslThree = X2.calc(aThree, bThree, cThree, xThree);
+        int rslThree = X2.calcThree(aThree, bThree, cThree, xThree);
         System.out.println(rslThree);
-        int aFour = 0;
+        int aFour = 1;
         int bFour = 1;
         int cFour = 1;
-        int xFour = 1;
-        int rslFour = X2.calc(aFour, bFour, cFour, xFour);
+        int xFour = 0;
+        int rslFour = X2.calcFour(aFour, bFour, cFour, xFour);
         System.out.println(rslFour);
     }
 }
--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -9,18 +9,17 @@ public class X2Test {
 
     @Test
     public void whenA10B0C0X2Then40() {
-        /* Входные парамметры. Их будет 4 для данного случая*/
         int a = 10;
         int b = 0;
         int c = 0;
         int x = 2;
-        /* ожидаемое значение. Это всегда одна переменная. */
         int expected = 40;
-        /* вызов метода, который хотим проверить. */
-        /* Результат вычисления. Это всегда одна переменная. */
         int rsl = X2.calc(a, b, c, x);
-        /* срвнение полученного значения с ожидаемым */
         Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C1X1Then3() {
         int aOne = 1;
         int bOne = 1;
         int cOne = 1;
@@ -28,6 +27,10 @@ public class X2Test {
         int expectedOne = 3;
         int rslOne = X2.calc(aOne, bOne, cOne, xOne);
         Assert.assertEquals(expectedOne, rslOne);
+    }
+
+    @Test
+    public void whenA0B1C1X1Then2() {
         int aTwo = 0;
         int bTwo = 1;
         int cTwo = 1;
@@ -35,6 +38,10 @@ public class X2Test {
         int expectedTwo = 2;
         int rslTwo = X2.calc(aTwo, bTwo, cTwo, xTwo);
         Assert.assertEquals(expectedTwo, rslTwo);
+    }
+
+    @Test
+    public void whenA1B1C0X1Then2() {
         int aThree = 1;
         int bThree = 1;
         int cThree = 0;
@@ -42,6 +49,10 @@ public class X2Test {
         int expectedThree = 2;
         int rslThree = X2.calc(aThree, bThree, cThree, xThree);
         Assert.assertEquals(expectedThree, rslThree);
+    }
+
+    @Test
+    public void whenA1B1C1X0Then1() {
         int aFour = 1;
         int bFour = 1;
         int cFour = 1;
