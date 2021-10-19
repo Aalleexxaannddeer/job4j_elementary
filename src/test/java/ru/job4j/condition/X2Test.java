6b9f331c503574a948b108f1180823ff049c6439
Aalleexxaannddeer <Alakey1998ssss@mail.ru>
2021-10-19 13:05:00 +0300

    1.2. Входные данные в тесте [#297750 #217091]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -19,46 +19,46 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X1Then3() {
-        int aOne = 1;
-        int bOne = 1;
-        int cOne = 1;
-        int xOne = 1;
-        int expectedOne = 3;
-        int rslOne = X2.calc(aOne, bOne, cOne, xOne);
-        Assert.assertEquals(expectedOne, rslOne);
+    public void whenA1B1C1X1When3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
     }
 
     @Test
     public void whenA0B1C1X1Then2() {
-        int aTwo = 0;
-        int bTwo = 1;
-        int cTwo = 1;
-        int xTwo = 1;
-        int expectedTwo = 2;
-        int rslTwo = X2.calc(aTwo, bTwo, cTwo, xTwo);
-        Assert.assertEquals(expectedTwo, rslTwo);
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
     }
 
     @Test
     public void whenA1B1C0X1Then2() {
-        int aThree = 1;
-        int bThree = 1;
-        int cThree = 0;
-        int xThree = 1;
-        int expectedThree = 2;
-        int rslThree = X2.calc(aThree, bThree, cThree, xThree);
-        Assert.assertEquals(expectedThree, rslThree);
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
     }
 
     @Test
     public void whenA1B1C1X0Then1() {
-        int aFour = 1;
-        int bFour = 1;
-        int cFour = 1;
-        int xFour = 0;
-        int expectedFour = 1;
-        int rslFour = X2.calc(aFour, bFour, cFour, xFour);
-        Assert.assertEquals(expectedFour, rslFour);
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
     }
 }
