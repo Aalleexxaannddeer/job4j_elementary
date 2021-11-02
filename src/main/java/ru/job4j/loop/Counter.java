12af333cd484da4f3dfd12ec2e786155888dce03
Aalleexxaannddeer <Alakey1998ssss@mail.ru>
2021-11-02 15:38:27 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192 #223227]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,17 +3,17 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            sum = i + sum;
+        for (int one = start; one <= finish; one++) {
+            sum = one + sum;
         }
         return sum;
     }
 
     public static int sumByEven(int start, int finish) {
         int sum = 0;
-        for (int e = start; e <= finish; e++) {
-            if (e % 2 == 0) {
-                sum = e + sum;
+        for (int two = start; two <= finish; two++) {
+            if (two % 2 == 0) {
+                sum = two + sum;
             }
         }
         return sum;
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -8,20 +8,29 @@ import static org.junit.Assert.*;
 public class CounterTest {
 
     @Test
-    public void sum() {
-        int i = 0;
+    public void whenSumEvenNumbersFromZeroToTenThenFiftyFive() {
+        int one = 0;
         int finish = 10;
-        int result = Counter.sum(i, finish);
+        int result = Counter.sum(one, finish);
         int expected = 55;
         Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void sumByEven() {
-        int e = 0;
+    public void whenSumByEvenNumbersFromZeroToTenThenthirty() {
+        int two = 0;
         int finish = 10;
-        int result = Counter.sumByEven(e, finish);
+        int result = Counter.sumByEven(two, finish);
         int expected = 30;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenSumByEvenNumbersFromZeroToTenThenEighteen() {
+        int three = 3;
+        int finish = 8;
+        int result = Counter.sumByEven(three, finish);
+        int expected = 18;
         Assert.assertEquals(expected, result);
     }
 }
