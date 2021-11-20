6afaa2971341b05e7e46cb547e704d8d31476d31
Aalleexxaannddeer <Alakey1998ssss@mail.ru>
2021-11-20 07:18:34 +0300

    6.0.5. Массивы в тестах. [#226789]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -6,7 +6,7 @@ public class Square {
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int index = 0; index < bound; index++) {
-            rst[index] = index * index;
+            rst[index] = index * index;e
         }
         return rst;
     }
