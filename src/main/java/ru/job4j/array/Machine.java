a31249bd77be2411f15eb5c24988ef7aacbebbae
Aalleexxaannddeer <Alakey1998ssss@mail.ru>
2022-01-10 06:31:32 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #245349]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 import java.util.Arrays;
 public class Machine {
-    public static int[] change(int money, int price) {
+    public static int[] change(int money, int price) {w
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
--- src/test/java/ru/job4j/array/MachineTest.java
+++ src/test/java/ru/job4j/array/MachineTest.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*;w
 
 public class MachineTest {
 
